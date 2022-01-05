7271948c2a9653320dee2e5e46203c1d914f41fb
Sunghee Yun <sunghee.yun@gmail.com>
2022-01-04 20:55:58 -0800

    linting

--- python/bin/youtube_channel_symbol.py
+++ python/bin/youtube_channel_symbol.py
@@ -10,29 +10,50 @@ if __name__ == "__main__":
 
     fig, axis = plt.subplots(figsize=(12, 12))
 
-    axis.text(0.1 - 0.5, 0.7, "MATH", size=70, rotation=5.,
-              ha="left", va="center",
-              bbox=dict(boxstyle="round",
-                       ec="none",
-                       fc=(0.4, 0.4, 1.0),
-                       )
-              )
+    axis.text(
+        0.1 - 0.5,
+        0.7,
+        "MATH",
+        size=70,
+        rotation=5.0,
+        ha="left",
+        va="center",
+        bbox=dict(
+            boxstyle="round",
+            ec="none",
+            fc=(0.4, 0.4, 1.0),
+        ),
+    )
 
-    axis.text(1.1, 0.5, "is", size=50, rotation=-5.,
-             ha="center", va="center",
-             bbox=dict(boxstyle="square",
-                       ec="none",
-                       fc=(1., 0.9, 0.9),
-                       )
-             )
+    axis.text(
+        1.1,
+        0.5,
+        "is",
+        size=50,
+        rotation=-5.0,
+        ha="center",
+        va="center",
+        bbox=dict(
+            boxstyle="square",
+            ec="none",
+            fc=(1.0, 0.9, 0.9),
+        ),
+    )
 
-    axis.text(1.9 + 0.5, 0.3, "FUN", size=70, rotation=5.,
-             ha="right", va="center",
-             bbox=dict(boxstyle="round",
-                       ec="none",
-                       fc=(0.4, 0.4, 1.0),
-                       )
-             )
+    axis.text(
+        1.9 + 0.5,
+        0.3,
+        "FUN",
+        size=70,
+        rotation=5.0,
+        ha="right",
+        va="center",
+        bbox=dict(
+            boxstyle="round",
+            ec="none",
+            fc=(0.4, 0.4, 1.0),
+        ),
+    )
 
     axis.axis("off")
 
--- python/number_paths.py
+++ python/number_paths.py
@@ -12,25 +12,58 @@ class ChessBoard:
         self.width: int = width
         self.height: int = height
 
-    def calculate_num_paths(self, goal: tp.Tuple[int, int]) -> tp.List[tp.List[int]]:
+    def calculate_num_paths(
+        self, goal: tp.Tuple[int, int]
+    ) -> tp.List[tp.List[int]]:
 
-        count_array_2d: tp.List[tp.List[int]] = [[0] * self.height for _ in range(self.width)]
+        count_array_2d: tp.List[tp.List[int]] = [
+            [None] * self.height for _ in range(self.width)
+        ]
 
         for col_idx in range(self.width):
             for row_idx in range(self.height):
-                self.calc_num_paths_for_one_cell(col_idx, row_idx, goal, count_array_2d)
+                self.calc_num_paths_for_one_cell(
+                    col_idx, row_idx, goal, count_array_2d
+                )
 
         for row_idx in range(self.height - 1, -1, -1):
-            print(" ".join([f"{str(count_array_2d[col_idx][row_idx]):>3}" for col_idx in range(self.width)]))
+            print(
+                " ".join(
+                    [
+                        f"{str(count_array_2d[col_idx][row_idx]):>6}"
+                        for col_idx in range(self.width)
+                    ]
+                )
+            )
 
-    def calc_num_paths_for_one_cell(self, col_idx: int, row_idx: int, goal: tp.Tuple[int, int], count_array_2d: tp.List[tp.List[int]]) -> None:
+        return count_array_2d
+
+    def calc_num_paths_for_one_cell(
+        self,
+        col_idx: int,
+        row_idx: int,
+        goal: tp.Tuple[int, int],
+        count_array_2d: tp.List[tp.List[int]],
+    ) -> None:
+
         val: int
-        if col_idx < 0 or col_idx >= self.width or row_idx < 0 or row_idx >= self.height:
+        if (
+            col_idx < 0
+            or col_idx >= self.width
+            or row_idx < 0
+            or row_idx >= self.height
+        ):
             return 0
+        elif count_array_2d[col_idx][row_idx] is not None:
+            val = count_array_2d[col_idx][row_idx]
         elif [col_idx, row_idx] == goal:
             val = 1
         else:
-            val = self.calc_num_paths_for_one_cell(col_idx - 1, row_idx + 1, goal, count_array_2d) + self.calc_num_paths_for_one_cell(col_idx + 1, row_idx + 1, goal, count_array_2d)
+            val = self.calc_num_paths_for_one_cell(
+                col_idx - 1, row_idx + 1, goal, count_array_2d
+            ) + self.calc_num_paths_for_one_cell(
+                col_idx + 1, row_idx + 1, goal, count_array_2d
+            )
 
         count_array_2d[col_idx][row_idx] = val
 
@@ -38,6 +71,6 @@ class ChessBoard:
 
 
 if __name__ == "__main__":
-    cb: ChessBoard = ChessBoard(8, 8)
+    cb: ChessBoard = ChessBoard(20, 20)
 
-    cb.calculate_num_paths([6, 7])
+    cb.calculate_num_paths([16, 19])
